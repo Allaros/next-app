e1885ab90b487764258f402fee81f96bcea6e946
Sergey Dubskiy <dubskiy_03@mail.ru>
2025-11-20 14:27:26 +0300

    Implement right and left sidebar

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -7,7 +7,7 @@ import { Theme } from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 dark:shadow-none">
       <Link href={"/"} className="flex items-center gap-1">
         <Image
           src={"/images/site-logo.svg"}
