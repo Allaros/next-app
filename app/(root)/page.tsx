349f33fa963caf2f880bb61603351f39ece8aff2
Sergey Dubskiy <dubskiy_03@mail.ru>
2025-11-24 20:12:40 +0300

    Implement logging

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,8 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import { handleError } from "@/lib/handlers/error";
+import dbConnect from "@/lib/mongoose";
 
 const questions: Question[] = [
   {
@@ -222,11 +224,22 @@ const questions: Question[] = [
   },
 ];
 
+const test = async () => {
+  try {
+    await dbConnect();
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const result = await test();
+  console.log(result);
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 
 import { RequestError, ValidationError } from "../http-errors";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -29,6 +30,11 @@ export const handleError = (
   responseType: ResponseType = "server"
 ) => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`
+    );
+
     return formatResponse(
       responseType,
       error.statusCode,
@@ -42,6 +48,8 @@ export const handleError = (
       error.flatten().fieldErrors as Record<string, string[]>
     );
 
+    logger.error({ err: error }, `ValidationError: ${validationError.message}`);
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -51,8 +59,10 @@ export const handleError = (
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
+
     return formatResponse(responseType, 500, error.message);
   }
-
+  logger.error({ err: error }, "An unexpected error occured");
   return formatResponse(responseType, 500, "An unexpected error occured");
 };
